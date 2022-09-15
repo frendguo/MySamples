1f8ecc5fd9374ba32854f61dfcaf863adb6e57a1
frendguo <frendguo@live.cn>
2022-09-15 16:17:57 +0800

    添加第一个WDM代码

---
+++ driver/WDM/FirstSample/HelloWDM/Sample.cpp
@@ -0,0 +1,34 @@
+#include <ntddk.h>
+
+void DriverUnload(
+	_In_ struct _DRIVER_OBJECT* DriverObject
+) {
+	UNREFERENCED_PARAMETER(DriverObject);
+
+	KdPrint(("Driver unload called\n"));
+}
+
+extern "C"
+NTSTATUS
+DriverEntry(_In_ PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath) {
+	UNREFERENCED_PARAMETER(DriverObject);
+	UNREFERENCED_PARAMETER(RegistryPath);
+
+	DriverObject->DriverUnload = DriverUnload;
+
+	RTL_OSVERSIONINFOW OsVersionInfo;
+	PRTL_OSVERSIONINFOW POsVersionInfo = &OsVersionInfo;
+	
+	NTSTATUS status = RtlGetVersion(_Out_ POsVersionInfo);
+	if (status == STATUS_SUCCESS && POsVersionInfo != NULL) {
+		KdPrint(("OS Version is %d.%d.%d\n",
+			POsVersionInfo->dwMajorVersion, POsVersionInfo->dwMinorVersion, POsVersionInfo->dwBuildNumber));
+	}
+	else {
+		KdPrint(("Get OsVersion fail. Status is %d\n", status));
+	}
+
+	KdPrint(("Driver initialized successfully\n"));
+
+	return STATUS_SUCCESS;
+}
