4ecea52c11c889e16d65bb10b26eacf1b6f81395
frendguo <frendguo@live.cn>
2022-09-28 08:57:54 +0800

    add CTL_CODE annotation.

--- driver/WDM/FirstSample/PriorityBooster/PriorityBoosterCommon.h
+++ driver/WDM/FirstSample/PriorityBooster/PriorityBoosterCommon.h
@@ -5,6 +5,10 @@
 
 #define PRIORITY_BOOSTER_DEVICE 0x8000
 
+// DeviceType: 一个常数。一般来自WDK头文件中定义的 FILE_DEVICE_xxx 常数。三方的值必须从 0x8000 开始
+// Function：一个往上增加的数字。这个驱动中，不同的控制代码必须不同。三方驱动必须从 0x800 开始
+// Method：标识输入/输出缓冲区是怎样传递给驱动的。包含 METHOD_BUFFERED、METHOD_IN_DIRECT、METHOD_OUT_DIRECT、METHOD_NEITHER
+// Access：指明这个操作是到达驱动程序（FILE_WRITE_ACCESS），来自驱动程序（FILE_READ_ACCESS），还是二者兼有（FILE_ANY_ACCESS）
 #define IOCTL_PRIORITY_BOOSTER_SET_PRIORITY CTL_CODE(PRIORITY_BOOSTER_DEVICE, \
 	0x800, METHOD_NEITHER, FILE_ANY_ACCESS)
 
