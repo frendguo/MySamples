e798ee6514f958e1c859102a9a77fa2bc5638917
guoshixin <guoshixin@cvte.com>
2023-09-25 11:59:52 +0800

    add console demo(suspend resume demo)

---
+++ console/SuspendResumeDemo/SuspendResumeDemo/SuspendResumeDemo.cpp
@@ -0,0 +1,44 @@
+﻿// SuspendResumeDemo.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#include <cstdio>
+#include <Windows.h>
+
+void test() {
+    while (true)
+    {
+        printf("test\n");
+        Sleep(1000);
+    }
+}
+
+int main()
+{
+    auto handle = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)test, NULL, 0, NULL);
+
+    if (handle == NULL) {
+        printf("CreateThread failed (%d)\n", GetLastError());
+        return 1;
+    }
+
+    Sleep(3000); // 让线程运行 3 秒
+
+    if (SuspendThread(handle) == -1) {
+        printf("SuspendThread failed (%d)\n", GetLastError());
+        return 1;
+    }
+
+    printf("SuspendThread success\n");
+
+    Sleep(3000); // 挂起 3 秒
+
+    if (ResumeThread(handle) == -1) {
+        printf("ResumeThread failed (%d)\n", GetLastError());
+        return 1;
+    }
+
+    printf("ResumeThread success\n");
+
+    CloseHandle(handle);
+
+    return 0;
+}
