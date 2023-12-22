0a2943f19d40032c54422e7a072eba44a5329c9d
guoshixin <guoshixin@cvte.com>
2023-12-22 17:29:01 +0800

    add hookdll demo

---
+++ console/MonitorCaptureHookDemo/HookDll/dllmain.cpp
@@ -0,0 +1,51 @@
+﻿// dllmain.cpp : Defines the entry point for the DLL application.
+#include "pch.h"
+
+#include <Windows.h>
+#include <detours/detours.h>
+
+static BOOL(WINAPI* TrueBitBlt)(HDC, int, int, int, int, HDC, int, int, DWORD) = BitBlt;
+
+BOOL WINAPI MyBitBlt(
+    HDC     hdcDest,
+    int     nXDest,
+    int     nYDest,
+    int     nWidth,
+    int     nHeight,
+    HDC     hdcSrc,
+    int     nXSrc,
+    int     nYSrc,
+    DWORD   dwRop)
+{
+    MessageBox(NULL, L"BitBlt Hooked...", L"BitBlt", MB_OK);
+    return TRUE;
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule,
+    DWORD  ul_reason_for_call,
+    LPVOID lpReserved
+)
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+        break;
+    case DLL_THREAD_ATTACH:
+        DetourRestoreAfterWith();
+
+        DetourTransactionBegin();
+        DetourUpdateThread(GetCurrentThread());
+        DetourAttach(&(PVOID&)TrueBitBlt, MyBitBlt);
+        DetourTransactionCommit();
+        break;
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        /*DetourTransactionBegin();
+        DetourUpdateThread(GetCurrentThread());
+        DetourDetach(&(PVOID&)TrueBitBlt, MyBitBlt);
+        DetourTransactionCommit();*/
+        break;
+    }
+    return TRUE;
+}
+
---
+++ console/MonitorCaptureHookDemo/MonitorCaptureHookDemo/MonitorCaptureHookDemo.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+int main()
+{
+    LPCSTR dllPath = "D:\\HookDll.dll";
+    DWORD pid = 48240;
+    HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pid);
+    if (hProcess == NULL)
+    {
+        std::cout << "OpenProcess failed--" << GetLastError() << std::endl;
+        return 1;
+    }
+
+    LPVOID pDllPath = VirtualAllocEx(hProcess, 0, strlen(dllPath) + 1, MEM_COMMIT, PAGE_READWRITE);
+    if (pDllPath == NULL)
+    {
+        std::cout << "VirtualAllocEx failed--" << GetLastError() << std::endl;
+        return 1;
+    }
+
+    // 写入DLL路径
+    WriteProcessMemory(hProcess, pDllPath, (LPVOID)dllPath, strlen(dllPath) + 1, 0);
+
+    // 创建远程线程
+    HANDLE hThread = CreateRemoteThread(hProcess, 0, 0, (LPTHREAD_START_ROUTINE)LoadLibraryA, pDllPath, 0, 0);
+
+    // 等待远程线程结束
+    WaitForSingleObject(hThread, INFINITE);
+
+    // 清理
+    VirtualFreeEx(hProcess, pDllPath, strlen(dllPath) + 1, MEM_RELEASE);
+    CloseHandle(hThread);
+    CloseHandle(hProcess);
+
+    return 0;
+}
