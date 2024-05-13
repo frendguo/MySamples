81f8735dce2acc77b4effa166b1f4b503022fe87
frendguo <frendguo@live.cn>
2024-05-13 16:16:52 +0800

    增加内存信息的获取

--- console/MemoryCleanDemo/MemoryCleanDemo/MemoryCleanDemo.cpp
+++ console/MemoryCleanDemo/MemoryCleanDemo/MemoryCleanDemo.cpp
@@ -147,15 +147,61 @@ bool EnablePrivilege() {
     return bRet;
 }
 
+typedef struct _MEMORY_OBJECT
+{
+    ULONG64 total_bytes;
+    ULONG64 free_bytes;
+    ULONG64 used_bytes;
+    ULONG percent;
+} MEMORY_OBJECT, * PMEMORY_OBJECT;
+
+typedef struct _MEMORY_INFO
+{
+    MEMORY_OBJECT physical_memory;
+    MEMORY_OBJECT virtual_memory;
+    MEMORY_OBJECT system_cache;
+} MEMORY_INFO, * PMEMORY_INFO;
+
+void GetMemoryInfo(PMEMORY_INFO memInfo) {
+    MEMORYSTATUSEX statex;
+    statex.dwLength = sizeof(statex);
+    SYSTEM_FILECACHE_INFORMATION sfci = { 0 };
+
+    if (GlobalMemoryStatusEx(&statex)) {
+        // physical memory
+        memInfo->physical_memory.total_bytes = statex.ullTotalPhys;
+        memInfo->physical_memory.free_bytes = statex.ullAvailPhys;
+        memInfo->physical_memory.used_bytes = statex.ullTotalPhys - statex.ullAvailPhys;
+        memInfo->physical_memory.percent = (ULONG)(memInfo->physical_memory.used_bytes * 100 / memInfo->physical_memory.total_bytes);
+
+        // virtual memory
+        memInfo->virtual_memory.total_bytes = statex.ullTotalVirtual;
+        memInfo->virtual_memory.free_bytes = statex.ullAvailVirtual;
+        memInfo->virtual_memory.used_bytes = statex.ullTotalVirtual - statex.ullAvailVirtual;
+    }
+
+    // system cache information
+    NTSTATUS status = NtQuerySystemInformation(SystemFileCacheInformation, &sfci, sizeof(sfci), NULL);
+    if (NT_SUCCESS(status)) {
+        memInfo->system_cache.total_bytes = sfci.CurrentSize;
+        memInfo->system_cache.free_bytes = sfci.PeakSize;
+        memInfo->system_cache.used_bytes = sfci.PeakSize - sfci.CurrentSize;
+    }
+}
+
 int main()
 {
     NTSTATUS status = ERROR_SUCCESS;
 
     EnablePrivilege();
 
+    // 获取内存信息
+    MEMORY_INFO beforMemInfo = { 0 };
+    GetMemoryInfo(&beforMemInfo);
+
     // 1. 清理系统文件缓存
     status = CleanSystemFileCache();
-    if (status == 0) {
+    if (NT_SUCCESS(status)) {
         printf("CleanSystemFileCache success\n");
     }
     else {
@@ -165,7 +211,7 @@ int main()
 
     // 2. 清空进程工作集
     status = SystemEmptyWorkingSet();
-    if (status == 0) {
+    if (NT_SUCCESS(status)) {
         printf("SystemEmptyWorkingSet success\n");
     }
     else {
@@ -174,7 +220,7 @@ int main()
 
     // 3. 清理低优先级的等待列表(standby list)
     status = PurgeLowPriorityStandbyList();
-    if (status == 0) {
+    if (NT_SUCCESS(status)) {
         printf("PurgeLowPriorityStandbyList success\n");
     }
     else {
@@ -183,7 +229,7 @@ int main()
 
     // 4. 清理内存中的等待列表(standby list)
     status = PurgeStandbyList();
-    if (status == 0) {
+    if (NT_SUCCESS(status)) {
         printf("PurgeStandbyList success\n");
     }
     else {
@@ -192,7 +238,7 @@ int main()
 
     // 5. 整理内存修改列表（modify list）
     status = FlushModifyedList();
-    if (status == 0) {
+    if (NT_SUCCESS(status)) {
         printf("FlushModifyedList success\n");
     }
     else {
@@ -201,7 +247,7 @@ int main()
 
     // 6. 将注册表 flush 到磁盘
     status = FlushRegistry();
-    if (status == 0) {
+    if (NT_SUCCESS(status)) {
         printf("FlushRegistry success\n");
     }
     else {
@@ -210,13 +256,30 @@ int main()
 
     // 7. 组合多个物理页面
     status = CombinePhysicalMemory();
-    if (status == 0) {
+    if (NT_SUCCESS(status)) {
         printf("CombinePhysicalMemory success\n");
     }
     else {
         printf("CombinePhysicalMemory failed, status: 0x%08X\n", status);
     }
     
+    printf("\n");
+    printf("Sleep 2s...\n");
+    Sleep(2 * 1000);
+
+    // 获取内存信息
+    MEMORY_INFO afterMemInfo = { 0 };
+    GetMemoryInfo(&afterMemInfo);
+
+    printf("Clear Result:\n");
+    printf("Physical Memory:\n");
+    std::cout << beforMemInfo.physical_memory.used_bytes / 1024.0 / 1024.0 << "MB -> " << afterMemInfo.physical_memory.used_bytes / 1024.0 / 1024.0 << "MB" << std::endl;
+    std::cout << beforMemInfo.physical_memory.percent << "% -> " << afterMemInfo.physical_memory.percent << "%" << std::endl;
+    printf("Virtual Memory:\n");
+    std::cout << beforMemInfo.virtual_memory.used_bytes / 1024.0 / 1024.0 << "MB -> " << afterMemInfo.virtual_memory.used_bytes / 1024.0 / 1024.0 << "MB" << std::endl;
+    printf("System Cache:\n");
+    std::cout << beforMemInfo.system_cache.used_bytes / 1024.0 / 1024.0 << "MB -> " << afterMemInfo.system_cache.used_bytes / 1024.0 / 1024.0 << "MB" << std::endl;
+
     system("pause");
 
     return 0;
