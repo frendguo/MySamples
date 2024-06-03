f16e36d6178ba57a1bded303db8907fac6cd90c8
frendguo <frendguo@live.cn>
2024-06-03 18:04:38 +0800

    增加遍历进程的对比

--- console/ProcessTypeDemo/ProcessTypeDemo/ProcessTypeDemo.cpp
+++ console/ProcessTypeDemo/ProcessTypeDemo/ProcessTypeDemo.cpp
@@ -8,6 +8,7 @@
 #include <conio.h>
 #include <psapi.h>
 #include <vector>
+#include <chrono>
 
 bool GetProcessExtendedBasicInformation(DWORD pid, PPROCESS_EXTENDED_BASIC_INFORMATION ppebi) {
     HANDLE hProcess = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_QUERY_LIMITED_INFORMATION, FALSE, pid);
@@ -206,35 +207,21 @@ std::wstring GetWindowState(HWND hwnd) {
     return L"Unknown";
 }
 
-void EnumProcess() {
-    HANDLE hProcess = GetCurrentProcess();
+void EnumProcess(std::vector<DWORD>& pids) {
+    HANDLE hProcess = nullptr;
     HANDLE nextProcess = NULL;
     while (NT_SUCCESS(NtGetNextProcess(hProcess, PROCESS_QUERY_INFORMATION, 0, 0, &nextProcess))) {
         auto pid = GetProcessId(nextProcess);
-        auto processName = GetProcessName(nextProcess);
-        std::wcout << "ProcessID: " << pid << ";ProcessName: " << processName << std::endl;
-        CloseHandle(hProcess);
+        pids.push_back(pid);
         hProcess = nextProcess;
     }
-
-    CloseHandle(hProcess);
 }
 
-int main()
-{
-    /*std::cout << "Please enter the PID of the process you want to check: " << std::endl;
-    DWORD pid;
-    std::cin >> pid;*/
-
-    EnumProcess();
-
-    std::vector<std::wstring> hasWindowprocess;
-    
-
+void EnumProcessBySnap(std::vector<DWORD>& pids) {
     HANDLE hProcessSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
     if (hProcessSnap == INVALID_HANDLE_VALUE) {
         std::cerr << "CreateToolhelp32Snapshot (of processes) failed: " << GetLastError() << std::endl;
-        return 1;
+        return;
     }
 
     PROCESSENTRY32 pe32;
@@ -244,35 +231,81 @@ int main()
     if (!Process32First(hProcessSnap, &pe32)) {
         std::cerr << "Process32First failed: " << GetLastError() << std::endl; // show cause of failure
         CloseHandle(hProcessSnap);          // clean the snapshot object
-        return 1;
+        return;
     }
 
     do
     {
-        // std::cout << "ProcessID: " << pe32.th32ProcessID << "IsBackground: " << IsBackgroudProcess(pe32.th32ProcessID) << std::endl;
-        std::wstring processName = GetProcessName(pe32.th32ProcessID);
-        if (processName == L"<unknown>") {
-            continue;
-        }
-        std::wcout << "Processname: " << processName << ";Subsystem: " << ConvertToSubsystemName(GetProcessSubsystem(pe32.th32ProcessID)) << std::endl;
-
-        auto windows = GetProcessWindows(pe32.th32ProcessID);
-        if (windows.size() < 1) {
-            continue;
-        }
-
-        hasWindowprocess.push_back(processName);
-        std::wcout << "Windows: " << std::endl;
-        for (auto hwnd : windows) {
-            std::wcout << "  Title: " << GetWindowTitle(hwnd) << ";State: " << GetWindowState(hwnd) << std::endl;
-        }
-
+        pids.push_back(pe32.th32ProcessID);
     } while (Process32Next(hProcessSnap, &pe32));
+}
 
-    std::wcout << "Processes with windows: " << std::endl;
-    for (std::wstring p : hasWindowprocess) {
-        std::wcout << "1111-" << p << std::endl;
-    }
+int main()
+{
+    /*std::cout << "Please enter the PID of the process you want to check: " << std::endl;
+    DWORD pid;
+    std::cin >> pid;*/
+    std::vector<DWORD> pids;
+    std::vector<DWORD> pids2;
+
+    auto start = std::chrono::high_resolution_clock::now();
+
+    EnumProcess(pids);
+
+    auto end = std::chrono::high_resolution_clock::now();
+
+    EnumProcessBySnap(pids2);
+
+    auto end2 = std::chrono::high_resolution_clock::now();
+
+    std::cout << "Time: " << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() << "us" << std::endl;
+    std::cout << "Time2: " << std::chrono::duration_cast<std::chrono::microseconds>(end2 - end).count() << "us" << std::endl;
+
+    //std::vector<std::wstring> hasWindowprocess;
+    //
+
+    //HANDLE hProcessSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+    //if (hProcessSnap == INVALID_HANDLE_VALUE) {
+    //    std::cerr << "CreateToolhelp32Snapshot (of processes) failed: " << GetLastError() << std::endl;
+    //    return 1;
+    //}
+
+    //PROCESSENTRY32 pe32;
+    //pe32.dwSize = sizeof(PROCESSENTRY32);
+
+    //// Retrieve information about the first process
+    //if (!Process32First(hProcessSnap, &pe32)) {
+    //    std::cerr << "Process32First failed: " << GetLastError() << std::endl; // show cause of failure
+    //    CloseHandle(hProcessSnap);          // clean the snapshot object
+    //    return 1;
+    //}
+
+    //do
+    //{
+    //    // std::cout << "ProcessID: " << pe32.th32ProcessID << "IsBackground: " << IsBackgroudProcess(pe32.th32ProcessID) << std::endl;
+    //    std::wstring processName = GetProcessName(pe32.th32ProcessID);
+    //    if (processName == L"<unknown>") {
+    //        continue;
+    //    }
+    //    std::wcout << "Processname: " << processName << ";Subsystem: " << ConvertToSubsystemName(GetProcessSubsystem(pe32.th32ProcessID)) << std::endl;
+
+    //    auto windows = GetProcessWindows(pe32.th32ProcessID);
+    //    if (windows.size() < 1) {
+    //        continue;
+    //    }
+
+    //    hasWindowprocess.push_back(processName);
+    //    std::wcout << "Windows: " << std::endl;
+    //    for (auto hwnd : windows) {
+    //        std::wcout << "  Title: " << GetWindowTitle(hwnd) << ";State: " << GetWindowState(hwnd) << std::endl;
+    //    }
+
+    //} while (Process32Next(hProcessSnap, &pe32));
+
+    //std::wcout << "Processes with windows: " << std::endl;
+    //for (std::wstring p : hasWindowprocess) {
+    //    std::wcout << "1111-" << p << std::endl;
+    //}
 
     system("pause");
     return 0;
